640168607693d0eb8450eb299a1f23436bab92cb
LukasMurdock <lukas.murdock@gmail.com>
2022-11-03 15:43:46 -0400

    update home

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,6 @@
 import Link from 'next/link'
+import { TestLinkSection } from '../ui/TestLinkSection'
 
 export default function Home() {
-    return (
-        <div>
-            <h1>Tests</h1>
-            <ul>
-                <li>
-                    <Link href="/server?test=">Server component</Link>
-                </li>
-                <li>
-                    <Link href="/client/nowork/?test=">Client component</Link>
-                </li>
-                <li>
-                    <Link href="/client/work/?test=">
-                        Client component that calls <code>useSearchParams</code>
-                    </Link>
-                </li>
-            </ul>
-        </div>
-    )
+    return <TestLinkSection />
 }
